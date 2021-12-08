232b440832771893f63eacb3a881a6bb66d5fffe
Matt F <trumpetman072392@gmail.com>
2021-12-08 11:23:24 -0600

    added greeting

--- TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
+++ TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
@@ -9,6 +9,8 @@ namespace TabloidCLI.UserInterfaceManagers
 
         public IUserInterfaceManager Execute()
         {
+
+            Console.WriteLine("Hey there cool cats and kittens");
             Console.WriteLine("Main Menu");
 
             Console.WriteLine(" 1) Journal Management");
